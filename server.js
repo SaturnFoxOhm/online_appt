5236c84eeac33fb3daa708981fc3b4abf698f8a5
Supakitt Surojanakul <81941466+SaturnFoxOhm@users.noreply.github.com>
2024-01-26 23:16:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'mydb'
+  password: 'ohm0817742474',
+  database: 'healthcheckupplatform'
 });
 
 var app = express();
@@ -39,17 +40,35 @@ app.post('/store-line-login-data', function (req, res, next) {
   console.log('Received Line Login data:', { lineUserId, displayName });
 
   connection.query(
-    'INSERT INTO `lineaccount` (`LineUserID`, `displayName`) VALUES (?, ?)', 
-    [lineUserId, displayName], 
+    'SELECT * FROM `lineaccount` WHERE `LineUserID` = ?',
+    [lineUserId],
     (error, results) => {
       if (error) {
-        console.error('Error updating Line Login data:', error);
+        console.error('Error checking existing data:', error);
         res.status(500).send('Internal Server Error');
       } else {
-        console.log('Line Login data stored successfully');
-        res.status(200).json({ message: 'Line Login data stored successfully' });
+        if (results.length > 0) {
+          // User already exists, do nothing
+          console.log('User already exists in the database');
+          res.status(200).json({ message: 'User already exists in the database' });
+        } else {
+          // User doesn't exist, insert into the database
+          connection.query(
+            'INSERT INTO `lineaccount` (`LineUserID`, `displayName`) VALUES (?, ?)',
+            [lineUserId, displayName],
+            (insertError, insertResults) => {
+              if (insertError) {
+                console.error('Error updating Line Login data:', insertError);
+                res.status(500).send('Internal Server Error');
+              } else {
+                console.log('Line Login data stored successfully');
+                // Send a redirect response to the client
+                res.status(302).send('Line Login data stored successfully');
+              }
+            }
+          );
+        }
       }
     }
   );
 });
-
