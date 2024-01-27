ba4e2fba253db831013b112dd497361bb43bc6f8
Supakitt Surojanakul <81941466+SaturnFoxOhm@users.noreply.github.com>
2024-01-28 00:24:10 +0700

    Add files via upload

---
+++ src/ProtectedRoute.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = async ({ element }) => {
+  // You can implement your authentication logic here
+  const response = await fetch('http://localhost:5000/user-auth', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    },
+    body: JSON.stringify(localStorage.getItem('token')),
+  });
+
+  if (response.status === 200) {
+    // User already exists, handle as needed
+    // Access the token from the response
+    isAuthenticated = false;
+  }
+  else if(response.status === 500){
+    isAuthenticated = true;
+  }
+
+  // If the user is authenticated, render the original element
+  // Otherwise, redirect to the login page
+  return isAuthenticated ? element : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
