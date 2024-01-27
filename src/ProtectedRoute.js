fd21ba8218057ade4388479e91cade49f2b08988
Supakitt Surojanakul <81941466+SaturnFoxOhm@users.noreply.github.com>
2024-01-28 02:00:56 +0700

    Update ProtectedRoute.js

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
+import liff from '@line/liff';
 
 const ProtectedRoute = ({ element, path }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isLoading, setIsLoading] = useState(true); // Add a loading state
+  const logout = () => {
+    liff.logout();
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
 
   useEffect(() => {
     const checkAuthentication = async () => {
@@ -22,6 +28,9 @@ const ProtectedRoute = ({ element, path }) => {
           setIsAuthenticated(true);
         } else {
           setIsAuthenticated(false);
+          liff.init({ liffId: '2002781192-5JV9lL87' }, () => {
+            logout();
+          }, err => console.error(err));
         }
       } catch (error) {
         console.error('Error while checking authentication:', error);
