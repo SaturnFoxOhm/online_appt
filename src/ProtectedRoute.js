8d8e3d3c4a7819019874583677b9684656ac7bee
Aummiez <aumatchaya@gmail.com>
2024-01-28 23:24:23 +0700

    Revert "Update ProtectedRoute.js"
    This reverts commit fd21ba8218057ade4388479e91cade49f2b08988.

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,15 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import liff from '@line/liff';
 
 const ProtectedRoute = ({ element, path }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isLoading, setIsLoading] = useState(true); // Add a loading state
-  const logout = () => {
-    liff.logout();
-    localStorage.removeItem('token');
-    window.location.reload();
-  }
 
   useEffect(() => {
     const checkAuthentication = async () => {
@@ -28,9 +22,6 @@ const ProtectedRoute = ({ element, path }) => {
           setIsAuthenticated(true);
         } else {
           setIsAuthenticated(false);
-          liff.init({ liffId: '2002781192-5JV9lL87' }, () => {
-            logout();
-          }, err => console.error(err));
         }
       } catch (error) {
         console.error('Error while checking authentication:', error);
