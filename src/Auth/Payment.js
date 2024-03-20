ac1cea2a0ac74b2c0742d03e8b992ea6ca33660b
SaturnFoxOhm <supakitt.sur@gmail.com>
2024-03-20 18:30:34 +0700

    runk ohm na

--- src/Auth/Payment.js
+++ src/Auth/Payment.js
@@ -64,7 +64,7 @@ const Payment = () => {
         }
     };
 
-    const [file, setFile] = useState<any>('');
+    const [file, setFile] = useState([]);
     const [previewUrl, setPreviewUrl] = useState([]);
 
     const handleFileChange = (e) => {
@@ -99,8 +99,8 @@ const Payment = () => {
         // const apiKey = '5bd4346e-a4d7-4177-8066-c324e2ed6602';
         try {
             const formData = new FormData();
-            formData.append('file', file);
-            console.log(file)
+            formData.append('file', file['File']);
+            console.log(file['File'])
             console.log(formData)
             const response = await axios.post('http://localhost:5000/check-slip', formData, {
                 method: 'POST',
