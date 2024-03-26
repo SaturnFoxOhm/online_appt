27c112806e3dfe99fe97afcd6b64e9c381c2458f
Supakitt Surojanakul <81941466+SaturnFoxOhm@users.noreply.github.com>
2024-03-26 17:14:23 +0700

    Update SignupAdmin.js

--- src/SuperAdmin/SignupAdmin.js
+++ src/SuperAdmin/SignupAdmin.js
@@ -40,13 +40,17 @@ const SignupAdmin = () => {
     const email = document.getElementById('email').value;
     const passwords = document.getElementById('passwords').value;
     const confirmPasswords = document.getElementById('confirmPasswords').value;
-    if(!AdminIDRegex.test(AdminID)){
-      alert('AdminID should be number only.');
+    if(!AdminIDRegex.test(AdminID) || AdminID.length !== 6){
+      alert('AdminID should be 6 digits long and contain only numbers.');
       return; // Do not proceed with signup
     }
     else if(!emailRegex.test(email)){
       alert('Invalid Email Format.');
       return; // Do not proceed with signup
+    }
+    else if (passwords.length > 20) {
+        alert('Password length should not exceed 20 characters.');
+        return; // Do not proceed with signup
     }
     else if (passwords !== confirmPasswords) {
       alert('Passwords do not match. Please enter matching passwords.');
@@ -160,7 +164,7 @@ const SignupAdmin = () => {
               <svg className="absolute ml-3 mt-1" width="24" viewBox="0 0 24 24">
                 <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z" />
               </svg>
-              <input type="test" id="AdminID" required className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="AdminID" />
+              <input type="test" id="AdminID" required className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="AdminID (Only 6 Numbers)" />
             </div>
             <div className="flex items-center text-lg mb-6 md:mb-8">
             <svg className="absolute ml-3 mt-1" width="24" viewBox="0 0 24 24">
@@ -172,7 +176,7 @@ const SignupAdmin = () => {
               <svg className="absolute ml-3 mt-1" viewBox="0 0 24 24" width="24">
                 <path d="m18.75 9h-.75v-3c0-3.309-2.691-6-6-6s-6 2.691-6 6v3h-.75c-1.24 0-2.25 1.009-2.25 2.25v10.5c0 1.241 1.01 2.25 2.25 2.25h13.5c1.24 0 2.25-1.009 2.25-2.25v-10.5c0-1.241-1.01-2.25-2.25-2.25zm-10.75-3c0-2.206 1.794-4 4-4s4 1.794 4 4v3h-8zm5 10.722v2.278c0 .552-.447 1-1 1s-1-.448-1-1v-2.278c-.595-.347-1-.985-1-1.722 0-1.103.897-2 2-2s2 .897 2 2c0 .737-.405 1.375-1 1.722z" />
               </svg>
-              <input type="password" id="passwords" required className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Password" />
+              <input type="password" id="passwords" required className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Password (Max 20 Characters)" />
             </div>
             <div className="flex items-center text-lg mb-4 md:mb-4">
               <svg className="absolute ml-3 mt-1" viewBox="0 0 24 24" width="24">
