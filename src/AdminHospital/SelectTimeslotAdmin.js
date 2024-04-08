a81f0bfce1920ffe962e74d60c8a53d8abde127c
Supakitt Surojanakul <supakitt.sur@gmail.com>
2024-04-09 00:21:46 +0700

    add selecttimeslot add for super admin and fix ux ui

--- src/AdminHospital/SelectTimeslotAdmin.js
+++ src/AdminHospital/SelectTimeslotAdmin.js
@@ -73,7 +73,9 @@ const PlaceAdmin = () => {
                 <div className="bg-gray-300 rounded shadow-lg p-4 px-4 md:p-6 mb-5">
                     <div className="grid gap-4 gap-y-2 text-m grid-cols-1 lg:grid-cols-1">
                         <div className="text-gray-600">
-                            <p className="font-large text-xl text-black whitespace-nowrap">Where do you want to edit Time Slot ? / คุณต้องการแก้ไข Time Slot ของที่ไหน ?</p>
+                            <p className="font-large text-xl text-black whitespace-normal">
+                                Where do you want to edit Time Slot ? / คุณต้องการแก้ไข Time Slot ของที่ไหน ?
+                            </p>
                             <br/>
                             <input type="radio" id="hospital" name="hospital" value="timeslothospital" onChange={handleOptionChange} checked={selectedOption === "timeslothospital"}/>
                             <label for="hospital" style={{ color: "black", marginLeft: "5px" }}>Hospital / เวลาทำงานในโรงพยาบาล</label><br/>
--- src/SuperAdmin/SelectTimeslotSuperAdmin.js
+++ src/SuperAdmin/SelectTimeslotSuperAdmin.js
@@ -4,10 +4,15 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const PlaceAdmin = () => {
     const [selectedOption, setSelectedOption] = useState('');
+    const [selectedOptionAdd, setSelectedOptionAdd] = useState('');
     const navigate = useNavigate();
 
     const handleOptionChange = (event) => {
         setSelectedOption(event.target.value);
+    };
+
+    const handleOptionAddChange = (event) => {
+        setSelectedOptionAdd(event.target.value);
     };
 
     const storePlace = () => {
@@ -32,6 +37,29 @@ const PlaceAdmin = () => {
             console.error('No place selected');
         }
     };
+
+    const storePlaceAdd = () => {
+        if (selectedOptionAdd) {
+            localStorage.setItem('selectedPlace', selectedOptionAdd);
+            console.log('Selected place stored in local storage:', selectedOptionAdd);
+
+            // Test localStorage
+            const testSelectedPlace = localStorage.getItem('selectedPlace');
+            console.log('SelectedPlace from local storage:', testSelectedPlace);
+
+            if (selectedOptionAdd === "addtimeslothospital") {
+                console.log('Redirecting...');
+                navigate('/super-admin/addtimeslothospital');
+            } 
+            else if (selectedOptionAdd === "addtimeslotoffsite") {
+                console.log('Redirecting...');
+                navigate('/super-admin/addtimeslotoffsite');
+            }
+        }
+        else {
+            console.error('No place selected');
+        }
+    };
     
     return (
         <div>
@@ -48,7 +76,7 @@ const PlaceAdmin = () => {
                 <div className="bg-gray-300 rounded shadow-lg p-4 px-4 md:p-6 mb-5">
                     <div className="grid gap-4 gap-y-2 text-m grid-cols-1 lg:grid-cols-1">
                         <div className="text-gray-600">
-                            <p className="font-large text-xl text-black whitespace-nowrap">Where do you want to edit Time Slot ? / คุณต้องการแก้ไข Time Slot ของที่ไหน ?</p>
+                            <p className="font-large text-xl text-black whitespace-normal">Where do you want to edit Time Slot ? / คุณต้องการแก้ไข Time Slot ของที่ไหน ?</p>
                             <br/>
                             <input type="radio" id="hospital" name="hospital" value="timeslothospital" onChange={handleOptionChange} checked={selectedOption === "timeslothospital"}/>
                             <label for="hospital" style={{ color: "black", marginLeft: "5px" }}>Hospital / เวลาทำงานในโรงพยาบาล</label><br/>
@@ -59,6 +87,28 @@ const PlaceAdmin = () => {
                         <div style={{ display: 'grid', placeItems: 'end' }}>
                             <button 
                             onClick={storePlace}
+                            className="self-end bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            >
+                            Next / ต่อไป
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="bg-gray-300 rounded shadow-lg p-4 px-4 md:p-6 mb-5">
+                    <div className="grid gap-4 gap-y-2 text-m grid-cols-1 lg:grid-cols-1">
+                        <div className="text-gray-600">
+                            <p className="font-large text-xl text-black whitespace-normal">Where do you want to add New Year Time Slot</p>
+                            <br/>
+                            <input type="radio" id="hospitaladd" name="hospitaladd" value="addtimeslothospital" onChange={handleOptionAddChange} checked={selectedOptionAdd === "addtimeslothospital"}/>
+                            <label for="hospitaladd" style={{ color: "black", marginLeft: "5px" }}>Hospital / เวลาทำงานในโรงพยาบาล</label><br/>
+                            <input type="radio" id="offsiteadd" name="offsiteadd" value="addtimeslotoffsite" onChange={handleOptionAddChange} checked={selectedOptionAdd === "addtimeslotoffsite"}/>
+                            <label for="offsiteadd" style={{ color: "black", marginLeft: "5px" }}>Off-site / เวลาทำงานนอกสถานที่</label><br/>
+                        </div>
+
+                        <div style={{ display: 'grid', placeItems: 'end' }}>
+                            <button 
+                            onClick={storePlaceAdd}
                             className="self-end bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                             >
                             Next / ต่อไป
